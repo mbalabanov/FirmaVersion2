700788fc92e2a97015262e47d990bd6e8318ed25
Marin Balabanov <marincomics@gmail.com>
2023-10-15 07:35:42 +0200

    First working version

--- src/at/bfi/projekt/firma_v2/Mitarbeiter.java
+++ src/at/bfi/projekt/firma_v2/Mitarbeiter.java
@@ -1,5 +1,131 @@
 package at.bfi.projekt.firma_v2;
 
-class Mitarbeiter {
+abstract class Mitarbeiter {
+
+	private int id;
+	private String name;
+
+	public Mitarbeiter() {
+	}
+
+	/**
+	 * @param id
+	 */
+	public Mitarbeiter(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @param id
+	 * @param name
+	 */
+	public Mitarbeiter(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public abstract double berechneBrutto();
+
+	/**
+	 * @param mitarbeiterListe
+	 * @param gesuchteId
+	 * @return
+	 */
+	public static Mitarbeiter suche(Mitarbeiter[] mitarbeiterListe, int gesuchteId) {
+		for (Mitarbeiter mitarbeiter : mitarbeiterListe) {
+			if (mitarbeiter.getId() == gesuchteId) {
+				return mitarbeiter;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return
+	 */
+	public String ausgabe() {
+		return "\nMitarbeiter\n===========\nId: " + id + "\nName: " + name + "\n";
+	}
+
+	/**
+	 * @param dieserMitarbeiter
+	 */
+	public static void ausgabe(Mitarbeiter dieserMitarbeiter) {
+		if (dieserMitarbeiter instanceof Arbeiter) {
+			System.out.println(dieserMitarbeiter.ausgabe());
+		}
+		if (dieserMitarbeiter instanceof Angestellter) {
+			System.out.println(dieserMitarbeiter.ausgabe());
+		}
+	}
+
+	/**
+	 * @param mitarbeiterListe
+	 */
+	public static void ausgabe(Mitarbeiter[] mitarbeiterListe) {
+		for (Mitarbeiter mitarbeiter : mitarbeiterListe) {
+			ausgabe(mitarbeiter);
+		}
+	}
+
+	/**
+	 * @param mitarbeiterListe
+	 * @return
+	 */
+	public static int getAnzAngestellterGesamt(Mitarbeiter[] mitarbeiterListe) {
+		int anzahlAngestellte = 0;
+
+		for (Mitarbeiter dieserMitarbeiter : mitarbeiterListe) {
+			if (dieserMitarbeiter instanceof Angestellter) {
+				anzahlAngestellte++;
+			}
+		}
+
+		return anzahlAngestellte;
+	};
+
+	/**
+	 * @param mitarbeiterListe
+	 * @return
+	 */
+	public static int getAnzArbeiterGesamt(Mitarbeiter[] mitarbeiterListe) {
+		int anzahlArbeiter = 0;
+
+		for (Mitarbeiter dieserMitarbeiter : mitarbeiterListe) {
+			if (dieserMitarbeiter instanceof Angestellter) {
+				anzahlArbeiter++;
+			}
+		}
+
+		return anzahlArbeiter;
+	};
 
 }
--- src/at/bfi/projekt/firma_v2/Verwaltung.java
+++ src/at/bfi/projekt/firma_v2/Verwaltung.java
@@ -1,7 +1,92 @@
 package at.bfi.projekt.firma_v2;
 
 class Verwaltung {
+
+	public static double summeAllerGehaelter;
+	public static final double TAX_ANG = 0.20;
+	public static final double TAX_ARB = 0.15;
+
 	public static void main(String[] args) {
 
+		final int SIZE = 12;
+
+		// Leeres Array fuer die Mitarbeiterliste
+		Mitarbeiter[] mitarbeiter_liste = new Mitarbeiter[SIZE];
+
+		// Erstellung der Arbeiterobjekte
+		Arbeiter arbeiter_0 = new Arbeiter(1, "Anneken", 29.1, 109, 94.4, 12.2);
+		Arbeiter arbeiter_1 = new Arbeiter(2, "Poldi", 31.7, 97, 101.3, 13.3);
+		Arbeiter arbeiter_2 = new Arbeiter(3, "Ottila", 17.2, 83, 44.9, 14.4);
+		Arbeiter arbeiter_3 = new Arbeiter(4, "Eckehard", 27.8, 134, 104.7, 15.5);
+		Arbeiter arbeiter_4 = new Arbeiter(5, "Juliane", 23.7, 124, 114.1, 16.6);
+		Arbeiter arbeiter_5 = new Arbeiter(6, "Erna", 22.5, 123, 111.8, 17.7);
+
+		// Erstellung der Angestelltenobjekte
+		Angestellter angestellter_6 = new Angestellter(7, "Mathis", 2400.00, 1211.30, 18.8);
+		Angestellter angestellter_7 = new Angestellter(8, "Hardwin", 2800.00, 1309.80, 19.9);
+		Angestellter angestellter_8 = new Angestellter(9, "Barthold", 1812.23, 992.20, 20.0);
+		Angestellter angestellter_9 = new Angestellter(10, "Ortrun", 1903.67, 1489.10, 21.1);
+		Angestellter angestellter_10 = new Angestellter(11, "Loreley", 1407.95, 1298.21, 22.2);
+		Angestellter angestellter_11 = new Angestellter(12, "Wilmar", 1703.67, 1496.21, 23.3);
+
+		// Zuweisung der Objekte zum Array mit der Liste der Mitarbeiter
+		mitarbeiter_liste[0] = arbeiter_0;
+		mitarbeiter_liste[1] = arbeiter_1;
+		mitarbeiter_liste[2] = arbeiter_2;
+		mitarbeiter_liste[3] = arbeiter_3;
+		mitarbeiter_liste[4] = arbeiter_4;
+		mitarbeiter_liste[5] = arbeiter_5;
+
+		mitarbeiter_liste[6] = angestellter_6;
+		mitarbeiter_liste[7] = angestellter_7;
+		mitarbeiter_liste[8] = angestellter_8;
+		mitarbeiter_liste[9] = angestellter_9;
+		mitarbeiter_liste[10] = angestellter_10;
+		mitarbeiter_liste[11] = angestellter_11;
+
+		// Suche nach Mitarbeiter anhand der ID
+		Mitarbeiter testMitarbeiter1 = Mitarbeiter.suche(mitarbeiter_liste, 3);
+		Mitarbeiter testMitarbeiter2 = Mitarbeiter.suche(mitarbeiter_liste, 8);
+
+		// Ausgabe der gefundenen Mitarbeiter
+		System.out.println("\nAusgabe einzelner Mitarbeiter,\nnachdem sie ueber die ID gefunden wurden:\n");
+		Mitarbeiter.ausgabe(testMitarbeiter1);
+		Mitarbeiter.ausgabe(testMitarbeiter2);
+
+		// Ausgabe der gesamten Mitarbeiterliste
+		System.out.println("\n\nAusgabe der gesamten MitarbeiterListe");
+		System.out.println("=========================================\n");
+
+		Mitarbeiter.ausgabe(mitarbeiter_liste);
+
+		// Ausgabe der Anzahl der Mitarbeiter (zuerst Angstellte dann Arbeiter)
+		System.out.print("\nAnzahl der Angestellten: ");
+		System.out.print(Mitarbeiter.getAnzAngestellterGesamt(mitarbeiter_liste));
+
+		System.out.println("\nAnzahl der Arbeiter: " + Mitarbeiter.getAnzArbeiterGesamt(mitarbeiter_liste));
+
+		berechneSummeAlleGehaelter(mitarbeiter_liste);
+		System.out.println("\nSumme aller Gehaelter: " + summeAllerGehaelter);
+
+		System.out.print("\nNettogehalt eines Arbeiters: ");
+		System.out.printf("%.2f%n", berechneNettoGehalt(mitarbeiter_liste[3]));
+
+		System.out.print("\nNettogehalt eines Angestellten: ");
+		System.out.printf("%.2f%n", berechneNettoGehalt(mitarbeiter_liste[11]));
+
 	}
+
+	public static void berechneSummeAlleGehaelter(Mitarbeiter[] mitarbeiter_liste) {
+		summeAllerGehaelter = 0.0;
+		for (Mitarbeiter dieserMitarbeiter : mitarbeiter_liste) {
+			summeAllerGehaelter += dieserMitarbeiter.berechneBrutto();
+		}
+	};
+
+	public static double berechneNettoGehalt(Mitarbeiter dieserMitarbeiter) {
+		double bruttoBetrag = dieserMitarbeiter.berechneBrutto();
+		double steuerBetrag = bruttoBetrag * TAX_ARB;
+
+		return bruttoBetrag - steuerBetrag;
+	};
 }
